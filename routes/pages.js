fc2f91c3a24661b5a51c0974ce061dc7166f4214
Arindam2002 <arindam2101@gmail.com>
2021-12-09 13:37:42 +0530

    .

--- routes/pages.js
+++ routes/pages.js
@@ -5,4 +5,8 @@ router.get("/home", function(req, res) {
   res.render("home");
 });
 
+router.get("/glass_partitions", function(req, res) {
+  res.render("glass_partitions");
+})
+
 module.exports = router;
